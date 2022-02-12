f51af6e7a96efc078034a72074fe02b1c73045fd
Vivek Subramanian <vman049@users.noreply.github.com>
2022-02-12 17:11:44 -0500

    Commenting out shuffling code since we are doing this offline

--- preprocess_dataset.sh
+++ preprocess_dataset.sh
@@ -47,17 +47,17 @@ for tl in $LANGUAGES
 do
 	echo "Shuffling $sl-$tl data"
 	
-	for l in $sl $tl
-	do
-		rm -f $DATA_PATH/$sl-$tl/shuffle_input.$l
-		rm -f $DATA_PATH/$sl-$tl/shuffle_output.$l
-		sed -n "1,${idx_test}p;$(($idx_test+1))q" $DATA_PATH/$sl-$tl/OpenSubtitles.$sl-$tl.$l > \
-		$DATA_PATH/$sl-$tl/shuffle_input.$l
-	done
+# 	for l in $sl $tl
+# 	do
+# 		rm -f $DATA_PATH/$sl-$tl/shuffle_input.$l
+# 		rm -f $DATA_PATH/$sl-$tl/shuffle_output.$l
+# 		sed -n "1,${idx_test}p;$(($idx_test+1))q" $DATA_PATH/$sl-$tl/OpenSubtitles.$sl-$tl.$l > \
+# 		$DATA_PATH/$sl-$tl/shuffle_input.$l
+# 	done
 	
-	bash shuffle_parallel_corpora.sh \
-	$DATA_PATH/$sl-$tl/shuffle_input.$sl $DATA_PATH/$sl-$tl/shuffle_input.$tl \
-	$DATA_PATH/$sl-$tl/shuffle_output.$sl $DATA_PATH/$sl-$tl/shuffle_output.$tl 42
+# 	bash shuffle_parallel_corpora.sh \
+# 	$DATA_PATH/$sl-$tl/shuffle_input.$sl $DATA_PATH/$sl-$tl/shuffle_input.$tl \
+# 	$DATA_PATH/$sl-$tl/shuffle_output.$sl $DATA_PATH/$sl-$tl/shuffle_output.$tl 42
 	
 	for l in $sl $tl
 	do
