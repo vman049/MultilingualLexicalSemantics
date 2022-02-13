8b053c1008ff4d7482e9f515c496a5dd10352972
Vivek Subramanian <vman049@users.noreply.github.com>
2022-02-12 19:55:29 -0500

    Removing torch, torchvision, and torchtext installation commands

--- train_txformer.sh
+++ train_txformer.sh
@@ -15,8 +15,6 @@ if [ ! -d OpenNMT-py ]; then
 	cd OpenNMT-py && git config core.filemode false
 	git checkout 14f3ac6f6cce7b8d9c77e51ea3b5c3de8f95b0d7
 	cd ..
-	pip install torch==1.4.0 torchvision==0.5.0
-	pip install torchtext==0.4.0
 fi
 
 sl=en
