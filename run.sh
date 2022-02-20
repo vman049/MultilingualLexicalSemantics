394fa09873f75544a23c201fca7f081a4057a7c9
Vivek Subramanian <vman049@users.noreply.github.com>
2022-02-19 22:57:10 -0500

    Commented out RNN training

--- run.sh
+++ run.sh
@@ -23,4 +23,4 @@ RESULTS_PATH=$5
 # bash create_dataset.sh $DATA_PATH "$LANGUAGES"
 bash preprocess_dataset.sh $DATA_PATH "$LANGUAGES" $SP_PATH $LD_LIBRARY_PATH_TEMP
 bash train_txformer.sh $DATA_PATH "$LANGUAGES" $RESULTS_PATH
-bash train_rnn.sh $DATA_PATH "$LANGUAGES" $RESULTS_PATH
+# bash train_rnn.sh $DATA_PATH "$LANGUAGES" $RESULTS_PATH
